d508874fb465e2681ccc94ddadf9260a2e2c81a5
Marvish <mschandr@uci.edu>
2022-07-27 17:59:02 -0700

    Initial commit

---
+++ AquaticSpecies/AquaticSpeciesApp.swift
@@ -0,0 +1,18 @@
+//
+//  AquaticSpeciesApp.swift
+//  AquaticSpecies
+//
+//  Created by Marvish Chandra on 7/27/22.
+//
+//
+
+import SwiftUI
+
+@main
+struct AquaticSpeciesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ aquatic.swift
@@ -0,0 +1,59 @@
+//
+// Created by Marvish Chandra on 7/27/22.
+// This program is intended to educate what aquatic shark species are dangerous or safe to humans.
+//
+
+import Foundation
+
+class Sharks {
+print("To first understand the shark species, you must understand the major types.")
+    func sharkSpecies():
+        totalList = ["Ground", "Requiem", "Bullhead", "Cow", "Mackarel", "Carpet", "Saws", "Dogfish", "Angel", "Prickly"]
+for s in totalList {allSharks = s print(allSharks)}
+
+}
+
+func safeSpecies(eatPrey)
+eatPrey = ["These shark species are not dangerous if it's consumption is limited to survive and doesn't harm humans."]
+print(eatPrey)
+safeSharks=["Whale":{0},"Nurse":{1},"Basking":{2},"Leopard":{3},"Angel":{4},"Bamboo":{5},"Goblin":{6},"Greenland":{7},"Sand Tiger":{8}]
+for safe in safeSharks {safeHuman = safe; print(safeHuman)}
+
+class veryDangerous {
+
+    func deadlySpecies();
+        deadlySharks = ["Great White": {
+0
+}
+,"Tiger":{
+    1},"Shortfin Mako":{
+    2},"Oceanic Whitetip":{
+    3},"Bull":{
+    4}]
+deadlySharks.forEach { d in
+    print(d)
+}
+
+// Determine if sharks are very dangerous based on more than 10 attacks
+
+gwAttacks = 314
+if gwAttacks > 10 {
+    print("The Great White shark is very dangerous to humans.")
+} else {print("The Great White shark is not dangerous to humans.")}
+tAttacks = 64
+if tAttacks > 10 {
+    print("The tiger shark is very dangerous to humans.")
+} else {print("The tiger shark is not dangerous to humans.")}
+smAttacks = 10
+if smAttacks > 10 {
+    print("The shortfin mako is very dangerous to humans.")
+} else{ print("The shortfin mako is not dangerous to humans.")}
+owAttacks = 15
+if owAttacks > 10 {
+    print("The oceanic whitetip is very dangerous to humans.")
+} else {print("The oceanic whitetip is not dangerous to humans.")}
+bullAttacks = 121
+if bullAttacks > 10 {
+    print("The bull shark is very dangerous to humans.")
+} else{ print("The bull shark is not dangerous to humans.")}
+
